5014e41ffc84bf67e5e5908b40aea2383dcbc454
Tim Heinsberg <timheinsberg@web.de>
2023-06-09 11:19:33 +0200

    Initial commit
    Versuch 2 bearbeitet und Versuch 3 aufgabe eins bearbeitet

---
+++ Versuch_2/sourcen/emain.c
@@ -0,0 +1,144 @@
+﻿#include "simuc.h"
+#include "io_treiber.h"
+
+
+// Die Verwendung der folgenden Zustandsnamen ist verbindlich
+typedef enum {hoch, runter, steht} STATE;
+
+// Die folgenden Defines muessen sinnvoll genutzt werden
+// Einige Defines zum leichteren Wiederfinden
+#define BIT_POS_IST_OBEN	0
+#define BIT_POS_IST_UNTEN	1
+#define BIT_POS_NACH_OBEN	4
+#define BIT_POS_NACH_UNTEN	5
+#define BIT_POS_NOTAUS 7
+
+#define BIT_POS_MOTOR_AN	0
+#define BIT_POS_FAHRE_NACH_OBEN	1
+#define BIT_POS_FAHRE_NACH_UNTEN	2
+#define BIT_POS_LED 3
+
+
+// Hier die Steuerungsfunktion definieren
+// ...
+void steuerungsfunktion(BYTE ist_oben,BYTE ist_unten,BYTE nach_oben,BYTE nach_unten,BYTE notaus,BYTE* fahre_nach_oben,BYTE* fahre_nach_unten,BYTE* led,STATE* state){
+    switch(*state){
+    case hoch:
+        //ausgabe signale bestimmen
+        *fahre_nach_oben = 1;
+        *fahre_nach_unten = 0;
+        *led = 1;
+
+        if(ist_oben == 1 || notaus == 1){
+            *state = steht;
+        }
+        break;
+    case runter:
+        //ausgabe signale bestimmen
+        *fahre_nach_oben = 0;
+        *fahre_nach_unten = 1;
+        *led = 1;
+
+        if(ist_unten == 1 || notaus == 1){
+            *state = steht;
+        }
+        break;
+    case steht:
+        //ausgabe signalebestimmen
+        *fahre_nach_oben = 0;
+        *fahre_nach_unten = 0;
+        *led = 0;
+        if((nach_oben == 1)&& (ist_oben == 0) && (notaus == 0)){
+            *state = hoch;
+        }else if((nach_unten == 1) && (ist_unten == 0) && (notaus == 0)){
+            *state = runter;
+        }
+        break;
+    default:
+        *state = runter;
+        break;
+    }
+}
+
+void emain(void* arg) 
+{
+
+    // Hier alle benoetigten Variablen deklarieren
+    //..
+    BYTE nach_unten,nach_oben,ist_unten,ist_oben,notaus;
+    BYTE  fahre_nach_unten = 0;
+    BYTE fahre_nach_oben = 0;
+    BYTE led = 0;
+
+    BYTE portC;
+    BYTE portD_nullMaske = ~(0 |(1 <<BIT_POS_FAHRE_NACH_UNTEN) | (1 <<BIT_POS_FAHRE_NACH_OBEN) | (1 <<BIT_POS_MOTOR_AN)); // Maske mit allen bits die editiert werden null
+    BYTE portD_output;
+    STATE state = steht;
+    //Variablen zum teste
+    /*
+    BYTE Port_outPut = 0;
+    unsigned short temp = 0;*/
+
+	INIT_BM_WITH_REGISTER_UI; // Hier unbedingt einen Break-Point setzen !!!
+
+	
+	// Hier die Treiberfunktionen aufrufen und testen (Aufgabe 1)
+    // ..
+    Init(&GlobalBoardHandle,0x9A);
+
+
+
+	// Ab hier beginnt die Endlosschleife fuer den Automaten (Aufgabe 2)
+    while(1) {
+
+		SYNC_SIM; 
+
+		// Hier die Eingabesignale einlesen
+		// ...
+        InputByte(GlobalBoardHandle,PC,&portC);
+        nach_unten = (portC>>BIT_POS_NACH_UNTEN) & 0x1;
+        nach_oben = (portC>>BIT_POS_NACH_OBEN) & 0x1;
+        ist_unten = (portC>>BIT_POS_IST_UNTEN) & 0x1;
+        ist_oben = (portC>>BIT_POS_IST_OBEN) & 0x1;
+        notaus = (portC>>BIT_POS_NOTAUS) & 0x1;
+        /*
+         //Test InputByte Methode
+        InputByte(BoardHandle,0,&Port_outPut);
+        InputByte(BoardHandle,1,&Port_outPut);
+        InputByte(BoardHandle,2,&Port_outPut);
+        InputByte(BoardHandle,3,&Port_outPut);
+        short temp = 0;
+        */
+        /*
+        //Test OutPutByte Methode
+        temp++;
+        OutputByte(BoardHandle,PC,temp);
+        temp++;
+        OutputByte(BoardHandle,PD,temp);
+        */
+
+
+
+		// Hier die Steuerungsfunktion aufrufen
+		// ...
+        steuerungsfunktion(ist_oben,ist_unten,nach_oben,nach_unten,notaus,&fahre_nach_oben,&fahre_nach_unten,&led,&state);
+
+
+
+		// Hier die Ausgabesignale ausgeben
+        //Port D einlesen
+        //um den prozess zu beschleunigen könnte man diese zeile auch einmal vor der endlos schleife einlesen und dann immer die werte des vorherigen durchlaufs verwenden,
+        //allerdings könnte es sein das die Bits noch anderweitig verwendet werden un deshalb lese ich sie zur sicherheit hier nochmal ein
+        InputByte(GlobalBoardHandle,PD,&portD_output);//Port D einlesen
+        portD_output = portD_output & portD_nullMaske;;//editierbaren bits auf 0 setzen
+        portD_output = portD_output | ((fahre_nach_unten <<BIT_POS_FAHRE_NACH_UNTEN) | (fahre_nach_oben <<BIT_POS_FAHRE_NACH_OBEN) | (led <<BIT_POS_LED) | (1<<BIT_POS_MOTOR_AN));
+         OutputByte(GlobalBoardHandle,PD,portD_output);
+
+	} // while(1)..
+
+	
+}
+
+
+
+  
---
+++ Versuch_2/sourcen/io_teiber.c
@@ -0,0 +1,207 @@
+﻿#include "simuc.h"
+#include "io_treiber.h"
+
+#include <stdio.h>
+
+typedef struct BHandle_Data {
+	BYTE Board_allocated;	// 1=allocated, 0=free
+	BYTE Port_A_Direction;	// 1=Output, 0=Input
+	BYTE Port_B_Direction;
+	BYTE Port_C_Direction;
+	BYTE Port_D_Direction;
+} BHandle;
+
+
+
+DSCB GlobalBoardHandle = NULL;  // Dies ist ein Zeiger (siehe typedef BHandle* DSCB in io_treinber.h),
+                                // der noch auf einen gueltigen Speicherbereich "gebogen" werden muss.
+                                // Dieser Speicherbereich muss dann in Init() mittels calloc() allokiert
+                                // werden.
+
+
+
+BYTE Init(DSCB* Zeiger_auf_BoardHandle, unsigned long int Steuerwort) {
+
+    DSCB Lokales_BoardHandle;   // Es wird empfohlen innerhalb dieser Funktion mit diesem
+                                // lokalen BoardHandle zu arbeiten. Ein Zugriff auf die
+                                // Stukturkomponenten ist dann ganz normal, z.B. mittels
+                                // Lokales_BoardHandle->Borad_allocated, moeglich.
+
+
+    if (*Zeiger_auf_BoardHandle == NULL){	
+        // Allokieren des Speichersbereiches und "biegen"  des lokalen Boadhandle auf diesen
+        // Speicherbereich.
+        Lokales_BoardHandle = (DSCB) calloc(1, sizeof(BHandle));
+
+        // Auch den per Call-By-Reference uebergebenen Zeiger auf den reservierten Speicherbereich "biegen"
+        *Zeiger_auf_BoardHandle = Lokales_BoardHandle;
+        Lokales_BoardHandle -> Board_allocated = 1;
+        GlobalBoardHandle = Lokales_BoardHandle;
+    }else{
+        Lokales_BoardHandle = *Zeiger_auf_BoardHandle;
+    }
+
+    // Ab hier nur noch mit dem lokalen BoardHande arbeiten
+    // Mit Leben fuellen
+    switch(Steuerwort){
+    case 0x92:
+        Lokales_BoardHandle->Port_A_Direction = 0;//Input
+        Lokales_BoardHandle->Port_B_Direction = 0;//Input
+        Lokales_BoardHandle->Port_C_Direction = 1;//Output
+        Lokales_BoardHandle->Port_D_Direction = 1;//Output
+        break;
+    case 0x93:
+        Lokales_BoardHandle->Port_A_Direction = 0;//Input
+        Lokales_BoardHandle->Port_B_Direction = 0;//Input
+        Lokales_BoardHandle->Port_C_Direction = 1;//Output
+        Lokales_BoardHandle->Port_D_Direction = 0;//Input
+        break;
+    case 0x9A:
+        Lokales_BoardHandle->Port_A_Direction = 0;//Input
+        Lokales_BoardHandle->Port_B_Direction = 0;//Input
+        Lokales_BoardHandle->Port_C_Direction = 0;//Input
+        Lokales_BoardHandle->Port_D_Direction = 1;//Output
+        break;
+    case 0x9B:
+        Lokales_BoardHandle->Port_A_Direction = 0;//Input
+        Lokales_BoardHandle->Port_B_Direction = 0;//Input
+        Lokales_BoardHandle->Port_C_Direction = 0;//Input
+        Lokales_BoardHandle->Port_D_Direction = 0;//Input
+        break;
+    default:
+        return 1;
+    }
+    //Ports configurieren
+
+
+    unsigned short dir = 0;
+
+    //Port 0 des Boards Konfiguieren
+        //Port A
+        if(Lokales_BoardHandle->Port_A_Direction == 1){//Port 0 Bit 0-7 als Output Konfigurieren
+            dir = dir | 0xFF;
+        }//Port 0 Bit 0-7 sind ohne das if bereits als input in dir konfiguriert
+
+        //Port B
+        if(Lokales_BoardHandle->Port_B_Direction == 1){//Port 0 Bit 8-15 als Output Konfigurieren
+            dir = dir | 0xFF00;
+        }//Port 0 Bit 8-15 sind ohne das if bereits als input in dir konfiguriert
+
+    //Dir Register von Port 0 konfigurieren
+        io_out16(DIR0,dir);
+
+    //Port 1 des Boards Konfigurieren
+        dir = 0;//hilfs variable wieder auf null setzen
+        //Port C
+        if(Lokales_BoardHandle->Port_C_Direction == 1){//Port 1 Bit 0-7 als Output Konfigurieren
+           dir = dir | 0xFF;
+        }//Port 1 Bit 0-7 sind ohne das if bereits als input in dir konfiguriert
+
+        //Port D
+        if(Lokales_BoardHandle->Port_D_Direction == 1){//Port 1 Bit 8-15 als Output Konfigurieren
+            dir = dir | 0xFF00;
+        }//Port 1 Bit 8-15 sind ohne das if bereits als input in dir konfiguriert
+    //Dir Register von Port 1 konfigurieren
+        io_out16(DIR1,dir);
+
+	return 0;
+}
+
+BYTE InputByte(DSCB BoardHandle, BYTE Port, BYTE *DigitalValue) {
+    
+    // Mit Leben fuellen
+
+    if(BoardHandle == NULL && BoardHandle->Board_allocated == 0){//ungültiges Board Handel
+        return 1;
+    }
+
+    switch(Port){
+        case PA:
+                *DigitalValue = io_in16(IN0);//Port 0 einlesen, das niederwertige bit wird automatisch isoliert da Byte nur ein Byte aufnehmen kann
+                return 0;
+            break;
+        case PB:
+                *DigitalValue = io_in16(IN0) >> 8;//Port 0 einlesen und das höherwertige bit isolieren
+                return 0;
+            break;
+
+        case PC:
+                *DigitalValue = io_in16(IN1);//Port eins einlesen , das niederwertige bit wird automatisch isoliert da Byte nur ein Byte aufnehmen kann
+                return 0;
+            break;
+        case PD:
+                *DigitalValue = io_in16(IN1)>> 8;//Port eins einlesen und das höherwertige bit isolieren
+                return 0;
+            break;
+        default:
+            return 2;
+    }
+}
+
+BYTE OutputByte(DSCB BoardHandle, BYTE Port, BYTE DigitalValue) {
+
+    if(BoardHandle == NULL && BoardHandle->Board_allocated == 0){//ungültiges Board Handel
+        return 1;
+    }
+
+    unsigned short portValue;
+    switch(Port){
+    case PA:
+        if(BoardHandle->Port_A_Direction == 1){//Ueberpruefen ob Port auf schreiben konfiguriert ist
+            portValue = io_in16(OUT0);//wert von out Put einlesen
+            portValue = portValue & 0xFF00;//niederwertigen Byte auf null setzen
+            portValue = portValue | DigitalValue;//Digital Value einsetzen
+            io_out16(OUT0,portValue);
+            return 0;
+        }else{
+            return 3;//Port ist nicht auf ausgang konfiguriert
+        }
+        break;
+    case PB:
+        if(BoardHandle->Port_B_Direction == 1){//Ueberpruefen ob Port auf schreiben konfiguriert ist
+            portValue = io_in16(OUT0);//wert von out Put einlesen
+            portValue = portValue & 0x00FF;//niederwertigen Byte auf null setzen
+            portValue = portValue | (DigitalValue<<8);//Digital Value einsetzen
+            io_out16(OUT0,portValue);
+            return 0;
+        }else{
+            return 3;//Port ist nicht auf ausgang konfiguriert
+        }
+        break;
+    case PC:
+        if(BoardHandle->Port_C_Direction == 1){//Ueberpruefen ob Port auf schreiben konfiguriert ist
+            portValue = io_in16(OUT1);//wert von out Put einlesen
+            portValue = portValue & 0xFF00;//niederwertigen Byte auf null setzen
+            portValue = portValue | DigitalValue;//Digital Value einsetzen
+            io_out16(OUT1,portValue);
+            return 0;
+        }else{
+            return 3;//Port ist nicht auf ausgang konfiguriert
+        }
+        break;
+    case PD:
+        if(BoardHandle->Port_D_Direction == 1){//Ueberpruefen ob Port auf schreiben konfiguriert ist
+            portValue = io_in16(OUT1);//wert von out Put einlesen
+            portValue = portValue & 0x00FF;//niederwertigen Byte auf null setzen
+            portValue = portValue | (DigitalValue<<8);//Digital Value einsetzen
+            io_out16(OUT1,portValue);
+            return 0;
+        }else{
+            return 3;//Port ist nicht auf ausgang konfiguriert
+        }
+        break;
+    default:
+        return 2;
+    }
+
+	return 0;
+}
+
+BYTE Free(DSCB BoardHandle) {
+    if(BoardHandle == NULL){//ungültiges Board Handel
+        return 1;
+    }
+    BoardHandle->Board_allocated = 0;
+	return 0;
+}
+
